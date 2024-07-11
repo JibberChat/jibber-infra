00e8d90aaf41f687989f7fddfd0b4816b1cd6420
aymene01 <70906904+aymene01@users.noreply.github.com>
2024-07-11 09:38:55 +0200

    Revert "feat: create ecr registry for all services (#3)"
    This reverts commit 1aec0cbe9f0e3409460f94f45cf706c704d65bbb.

--- ecr.tf
+++ ecr.tf
@@ -6,39 +6,3 @@ resource "aws_ecr_repository" "gateway_registry" {
     scan_on_push = true
   }
 }
-
-resource "aws_ecr_repository" "gateway_registry" {
-  name                 = "user-service-ecr-registry"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_repository" "gateway_registry" {
-  name                 = "chat-service-ecr-registry"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_repository" "gateway_registry" {
-  name                 = "notification-service-ecr-registry"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
-
-resource "aws_ecr_repository" "gateway_registry" {
-  name                 = "media-ecr-registry"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-}
